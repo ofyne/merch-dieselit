64d1c510c54392e0cd33514ac26b6e04be133817
ofyne <sociafober@gmail.com>
2026-09-15 18:45:04 +0200

    first commit

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,6 +1,1 @@
-export const formatPrice = (value: number) =>
-	new Intl.NumberFormat('ru-RU', {
-		style: 'currency',
-		currency: 'RUB',
-		maximumFractionDigits: 0,
-	}).format(value)
+export type { Product, ProductSize } from './product'
